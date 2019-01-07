97fef11696f00af7875de36ce14b49a118349bf7
shumake <shumake@phoenix-5.com>
2019-01-07 14:57:03 +0800

    eshop專案-新增sitemap

--- eshop/controllers/default.js
+++ eshop/controllers/default.js
@@ -1,5 +1,6 @@
 exports.install = function() {
 	F.route('/',list_product, ['*Products']);
+	F.route('/products',query_products,['*Products']);
 	F.route('/{price}', json_get, ['*Products']);
 	F.route('/',update_product,['post','*Products']);
 	F.route('/addproduct',add_product,['post','*Products'])
@@ -23,6 +24,15 @@ function list_product() {
 	var self = this;
 	
 	self.$query(self, self.callback('index'));
+}
+
+/**
+ * 查詢產品資料
+ */
+function query_products(){
+	var self = this;
+	
+	self.$query(self, self.callback('products'));
 }
 
 function json_get(price) {
